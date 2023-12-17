97e612f99cf62eb3c1251992580938d16352645c
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2023-12-17 19:02:18 +0530

    date added

--- newblog/post/models.py
+++ newblog/post/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100, null=True, blank=True)
     description = models.CharField(max_length=500, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     class Meta:
         db_table = "Post"
